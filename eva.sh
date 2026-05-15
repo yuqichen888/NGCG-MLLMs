cea1f93d88ab3ef3bd0577f2a159be0e55cf2f4d
Yuqi <cyukiki8@gmail.com>
2026-05-15 12:56:57 -0400

    Update eva.sh

--- eva.sh
+++ eva.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 #SBATCH --partition=nvgpu
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1  # torchrun handles processes
-#SBATCH --cpus-per-task=12    # Request enough CPUs for data loading, DDP, etc.
-#SBATCH --gres=gpu:1        # Request 2 GPUs
+#SBATCH --ntasks-per-node=1 
+#SBATCH --cpus-per-task=12   
+#SBATCH --gres=gpu:1       
 #SBATCH --mem=32G
 #SBATCH --time=01-23:59:59
 #SBATCH --job-name=eva
-#SBATCH --output=%x_%j_lvec-mini_drone_eva.out
+#SBATCH --output=%x_%j_eva.out
 #SBATCH --mail-user=ychen57@uvm.edu
 #SBATCH --mail-type=ALL
 
 
 source ~/scratch/miniconda3/etc/profile.d/conda.sh
-conda activate lightvec
+conda activate ngcg
 
 module load cuda/12.4.1
 
-cd /gpfs2/scratch/ychen57/code/LightVec
 python eva.py \
     --config_path ./config.yaml \
     --checkpoint_to_eval ./src/vlm_backbone/smolvlm/ \
